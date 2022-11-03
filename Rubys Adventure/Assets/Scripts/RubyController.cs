778f6ccb2dc1368a0c740b78bb6a2a4eb3e162da
nhyde2000 <nolanhyde2000@gmail.com>
2022-11-03 13:13:00 -0400

    first

---
+++ Rubys Adventure/Assets/Scripts/RubyController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+   // Start is called before the first frame update
+   void Start()
+   { 
+       //QualitySettings.vSyncCount = 0;
+       //Application.targetFrameRate = 10;
+   }
+   // Update is called once per frame
+   void Update()
+   {
+       float horizontal = Input.GetAxis("Horizontal");
+       float vertical = Input.GetAxis("Vertical");
+       Vector2 position = transform.position;
+       position.x = position.x + 0.1f * horizontal * Time.deltaTime;
+       position.y = position.y + 0.1f * vertical * Time.deltaTime;
+       transform.position = position;
+   }
+}
